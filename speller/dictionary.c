4bc33b40365f28ee71d64305a3bbf454b24dfd01
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-03-05 13:25:06 +0300

    load & hash fuction comleted

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -15,7 +15,7 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 262145; //максимальное uint, значительно больше размера словаря; ПОПРОБОВАТЬ 2^18+1 == 262145 ( & 0x3FFFF)
 
 // Hash table
 node *table[N];
@@ -28,44 +28,64 @@ bool check(const char *word)
 }
 
 // Hashes word to a number
-unsigned int hash(const char *word)
+//ROT13 Hash Function. No multiplication, by Serge Vakulenko. Two shifts are converted by
+// GCC 4 to a single rotation instruction. http://vak.ru/doku.php/proj/hash/sources
+unsigned int hash(const char *word) //Брать нужное количестов битов справа логическим И
 {
-    // TODO
-    return 0;
+	unsigned int hash = 0;
+	unsigned int i = 0;
+	while (word[i] != '\0')
+	{
+		hash += (unsigned char)(*word);
+		hash -= (hash << 13) | (hash >> 19);
+		word++;
+		i++;
+	}
+	return hash & 0x3FFFF; //выбираем только 19 правых разрядов, чтобы в него поместился словарь с количеством слов N
 }
+
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     //открыть файл словаря
-    FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
+    FILE *dict_file = fopen(dictionary, "r");
+    if (dict_file == NULL)
     {
         printf("Dictionary file %s hasn't been opened!\n", dictionary);
+        fclose(dict_file);
         return false;
     }
 
     //считать слова
     char buffer[LENGTH + 1] = {'\0'};
-    printf("In %s dictionary there are the following words: \n", dictionary);
-    while (fscanf(file, "%s", buffer) != EOF)
+    printf("There are the following words in %s: \n", dictionary);
+    while (fscanf(dict_file, "%s", buffer) != EOF)
     {
+
         //создаём новый node
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
-            printf("There is error in allocation of memory.\n");
+            printf("There is error in allocation of memory for new node.\n");
+            fclose(dict_file);
             return false;
         }
-        strcpy(n->word, buffer); //запоминаем слварное слово в node
-        n->next = n; //указываем на следующий node
-
-        printf("%s\n", n->word); //ПРОВЕРКА - выводим на экран что записали в node
-
+        n->next = NULL;
+        strcpy(n->word, buffer); //запоминаем словарное слово в новую ноду
+        n->next = table[hash(n->word)];
+        table[hash(n->word)] = n;
+        printf("%s\n", table[hash(n->word)]->word);
+        free(n);
+        int i = 0;
+        do
+        {
+            buffer[i] = '\0';
+            i++;
+        }
+        while (buffer[i] != '\0');
     }
-
-
-    fclose(file);
+    fclose(dict_file);
     return true;
 }
 
