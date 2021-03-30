86d7ec4589187704f71616bc11182a8e46daafbc
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-03-30 08:22:41 +0300

    Create credit.py. Init check_cardnum function.

---
+++ Python/credit.py
@@ -0,0 +1,21 @@
+def check_13to16(num):
+    """
+    Check if input string consists of 13 to 16 digits.
+    :param num: String which we want to check.
+    :return:
+    """
+
+    pass
+
+
+def check_cardnum(num: str) -> str:
+    """
+    Check if input is a real card number. Main function.
+    :param num: String which we want to check. Is it a card number?
+    :return: Name of card AMEX\n or MASTERCARD\n or VISA\n or INVALID\n
+    """
+    pass
+
+
+number = input("Number: ")
+print(check_cardnum(number))
