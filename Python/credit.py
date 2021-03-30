cd83a580d2491a323c5bb0e0bb9640438d29a0ef
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-03-30 09:13:36 +0300

    Complete check if input is 13 to 16 digits.

--- Python/credit.py
+++ Python/credit.py
@@ -1,10 +1,19 @@
-def check_13to16(num):
+import re
+
+def check_13to16(num: str) -> bool:
     """
     Check if input string consists of 13 to 16 digits.
     :param num: String which we want to check.
-    :return:
+    :return: True if input string consists of 13 to 16 digits.
     """
-    pass
+    mask_13to16 = re.compile('[0-9]{13,16}')
+    match_13to16 = mask_13to16.match(num)
+    mask_17 = re.compile('[0-9]{17}')
+    match_17 = mask_17.match(num)
+    if match_13to16 and (match_17 is None):
+        return True
+    else:
+        return False
 
 
 def check_cardnum(num: str) -> str:
@@ -14,7 +23,9 @@ def check_cardnum(num: str) -> str:
     :return: Name of card AMEX\n or MASTERCARD\n or VISA\n or INVALID\n
     """
     name_card = 'AMEX', 'MASTERCARD', 'VISA', 'INVALID'
-    if not check_13to16(num):
+    if check_13to16(num):
+        return "May be cardnumber."
+    else:
         return name_card[3]
 
 
