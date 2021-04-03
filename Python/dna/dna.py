52ac108741221151924b0bed19db91691d9e8224
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-04-03 10:09:26 +0300

    Functions input_data and input sequence prototyped

--- Python/dna/dna.py
+++ Python/dna/dna.py
@@ -1,6 +1,27 @@
 # Find man by his DNA.
 
 import sys
+import csv
+
+
+def input_data(database: str) -> dict:
+    """
+    Read database file.
+    :param database: Name of database csv-file.
+    :return: Dictionary of people.
+    """
+    # TODO: Complete the function.
+    return {'NotCompletedDictKey': 'NotCompletedDictValue'}
+
+
+def input_seq(sequence: str) -> str:
+    """
+    Read sequence file.
+    :param sequence: Name of sequence file.
+    :return: String of sequence.
+    """
+    # TODO: Complete the function.
+    return str('NotCompletedSequence')
 
 
 def check_dna(database: str, sequence: str) -> str:
@@ -11,6 +32,8 @@ def check_dna(database: str, sequence: str) -> str:
     :return: Name of man or "No match".
     """
     if len(sys.argv) == 3:
+        data = input_data(database)
+        seq = input_seq(sequence)
         name = 'NotYetCompleted Man'
         return name
     else:
