a08d037a9b112e6d1445d436d48eeadee51fca4e
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-04-03 09:54:51 +0300

    Check for right number of arguments added.

--- Python/dna/dna.py
+++ Python/dna/dna.py
@@ -3,18 +3,27 @@
 import sys
 
 
-def check_dna(database: str, sequences: str) -> str:
-    # TODO: Complete check of right number of arguments.
-    if len(sys.argv) != 3:
-        return "Usage: python dna.py data.csv sequence.txt"
-
-    name = 'NotYetCompleted Man'
-    return name
+def check_dna(database: str, sequence: str) -> str:
+    """
+    Check if DNA in sequence file belongs to any man from database file.
+    :param database: Name of database file.
+    :param sequence: Name of sequennce file.
+    :return: Name of man or "No match".
+    """
+    if len(sys.argv) == 3:
+        name = 'NotYetCompleted Man'
+        return name
+    else:
+        raise IndexError
 
 
 # MAIN PROGRAM
-man = check_dna(sys.argv[1], sys.argv[2])
-print(man)
+try:
+    man = check_dna(sys.argv[1], sys.argv[2])
+    print(man)
+except IndexError:
+    print("Usage: python dna.py data.csv sequence.txt")
+    exit(0)
 
 if __name__ == '__main__':
     check_dna(sys.argv[1], sys.argv[2])
