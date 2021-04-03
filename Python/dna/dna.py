3afa25abdc98b831c17ffc112f558818cecad0b3
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-04-03 07:38:16 +0300

    Main function check_dna prototyped.

---
+++ Python/dna/dna.py
@@ -0,0 +1,13 @@
+# Find man by his DNA.
+
+import sys
+def check_dna(database: str, sequences: str) -> str:
+    pass
+
+
+# MAIN PROGRAM
+man = check_dna(database, sequences)
+print(man)
+
+if __name__ == '__main__':
+    check_dna(args[1], args[2])
