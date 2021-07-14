d05e0c1c71b8587ca0e982dd46b340bf667ce3cd
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-07-14 08:58:16 +0300

    Param 'year' for footer is added.

--- LearnFlask/flsite.py
+++ LearnFlask/flsite.py
@@ -4,6 +4,7 @@ from jinja2 import FileSystemLoader, Environment
 app = Flask(__name__)
 
 menu = ["Главная", "О проекте", "Контакты"]
+year = 2021
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
@@ -12,18 +13,18 @@ env = Environment(loader=file_loader)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html', menu=menu)
+    return render_template('index.html', menu=menu, year=year)
 
 
 @app.route("/about")
 def about():
     cars = [1, 2, 3, 4, 5]
-    return render_template('about.html', title="О проекте", menu=menu, cars=cars)
+    return render_template('about.html', title="О проекте", menu=menu, year=year, cars=cars)
 
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html', title="Контакты", menu=menu)
+    return render_template('contact.html', title="Контакты", menu=menu, year=year)
 
 
 if __name__ == "__main__":
