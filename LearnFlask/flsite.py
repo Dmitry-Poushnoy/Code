f2a45c142da34de2d9bf0b6686c6027946a74422
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2021-07-14 08:50:27 +0300

    Substitution for <title> added

--- LearnFlask/flsite.py
+++ LearnFlask/flsite.py
@@ -12,17 +12,19 @@ env = Environment(loader=file_loader)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html', title="Главная страница", menu=menu)
+    return render_template('index.html', menu=menu)
+
 
 @app.route("/about")
 def about():
     cars = [1, 2, 3, 4, 5]
-    return render_template('about.html', title="Страница о проекте", menu=menu, cars=cars)
+    return render_template('about.html', title="О проекте", menu=menu, cars=cars)
 
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html', title="Контакты", menu=menu)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
